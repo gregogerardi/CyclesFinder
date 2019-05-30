ba8688dbc2b066f689663583d7f8abb5ef93b3d8
rdmarcos49 <roberto.dmarcos@hotmail.com>
2019-05-30 12:54:03 -0300

    cambios en el main, dependencieshandler, y printer agregado

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,30 +1,67 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
 class Printer {
 
-    static void print(List<List<String>> circuits, String path) throws IOException {
+    /**
+     * Volcado en archivos los ciclos encontrados mediante [Johnson]
+     *
+     * @param circuits de listas, que es el formato devuelto por [findCircuits()]
+     * @param path     absoluta donde se creara el archivo con los ciclos
+     */
 
-        BufferedWriter writer = new BufferedWriter(new FileWriter(path));
-        int num = 1;
-        for (List<String> ss : circuits) {
-            StringBuilder output = new StringBuilder();
-            output.append(num).append(") ");
-            int cont = 1;
-            for (String s : ss) {
-                output.append(s);
-                if (cont != ss.size())
-                    output.append(" -> ");
-                cont++;
-            }
-            writer.write(output.toString());
-            writer.newLine();
-            writer.newLine();
-            num++;
+    void print(List<List<String>> circuits, String path) throws IOException {
+
+        FileOutputStream fos = new FileOutputStream(path);
+        DataOutputStream dos = new DataOutputStream(fos);
+
+        if (circuits.size() == 0) {
+            byte disernible = 0;
+            dos.writeChar(disernible);
+            dos.close();
         }
-        writer.close();
+
+        int num = 1;
+        circuits.forEach(strings -> printFormato1(strings, dos, num));
+        dos.close();
+    }
+
+    private void printFormato1(List<String> strings, DataOutputStream dos, int num) {
+        try {
+            dos.writeChar(num);
+            dos.writeChar(')');
+            dos.writeChar(' ');
+            strings.forEach(palabra -> printFormato2(strings, dos, num));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void printFormato2(List<String> strings, DataOutputStream dos, int num) {
+        for (int i = 0; i < strings.size(); i++) {
+            try {
+                printTerminal(strings.get(i), dos);
+                if ((i + 1) != strings.get(i).length()) {
+                    dos.writeChar(' ');
+                    dos.writeChar('-');
+                    dos.writeChar('>');
+                    dos.writeChar(' ');
+                }
+                num++;
+                dos.writeChar('\n');
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void printTerminal(String palabra, DataOutputStream dos) throws IOException {
+        for (int i = 0; i < palabra.length(); i++) {
+            dos.writeChar(palabra.charAt(i));
+        }
     }
 
 }
